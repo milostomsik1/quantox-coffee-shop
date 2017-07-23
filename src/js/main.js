90fe5115813631536c9554cc4bc52271be103b88
milostomsik1 <milostomsik1@gmail.com>
2017-07-23 02:04:23 +0200

    added google maps, markers for user and coffee shops, did v1, v2 and v3 of UI design

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,19 @@
 //Foursquare API auth
-var CLIENT_ID = "SRSTKKDUOVCGTIYWEDEXYFGPPCFWSE25KA3PIJ3MLR0UFRWK"; //ES6 const
-var CLIENT_SECRET = "CMTPSXLN4WZL0W2GT2GIYADXUKXFPAUKXDBKFYFXQHASS4KV"; //ES6 const
+var CLIENT_ID = "SRSTKKDUOVCGTIYWEDEXYFGPPCFWSE25KA3PIJ3MLR0UFRWK"; //ES6 CONST
+var CLIENT_SECRET = "CMTPSXLN4WZL0W2GT2GIYADXUKXFPAUKXDBKFYFXQHASS4KV"; //ES6 CONST
+// var GOOGLE_MAPS_API_KEY = "AIzaSyA4L_QGX6E5Kb2ggRi_B0ijnBUTBSGSx4g" //ES6 CONST
 
-//GEOLOCATION
+//VARS
 var userLatitude;
 var userLongitude;
+var map;
+var uluru;
+var coffeeShops = [];
 
+
+//GEOLOCATION API
+
+//RUNS GEOLOCATION ON LOAD
 (function getLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(getLatLng, getError, {enableHighAccuracy: true, timeout: 5000});
@@ -15,12 +23,15 @@ var userLongitude;
 	}
 })();
 
+//RETURNING COORDS AND CALLING SEARCH FUNCTION
 function getLatLng(position) {
 	userLatitude = position.coords.latitude;
 	userLongitude = position.coords.longitude;
 	console.log(userLatitude + " " + userLongitude);
 
 	searchForCoffeeShops(userLatitude, userLongitude);
+	initMap(userLatitude, userLongitude);
+	createUserMarker(userLatitude, userLongitude);
 }
 
 function getError(error) {
@@ -41,12 +52,11 @@ function getError(error) {
 }
 
 //FOURSQUARE API
-var coffeeShops = [];
 
 //SEARCHES FOR UP TO 10 COFFEE SHOPS WITHIN 1000M RADIUS
 function searchForCoffeeShops(lat, lng) {
 	var venuesRequested = 10;
-	var searchRadius = 1500;
+	var searchRadius = 1500;  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< VRATITI NA 1000 METARA
 	var searchVenuesURL =	"https://api.foursquare.com/v2/venues/search?" +
 									"client_id=" + CLIENT_ID +
 									"&client_secret=" + CLIENT_SECRET +
@@ -101,37 +111,71 @@ function getCoffeeShopInfo(coffeeShopId, coffeeShopDistance) {
 				}
 			}
 			var coffeeShopPicture = coffeeShop.photos.groups["0"].items["0"].prefix + "original" + coffeeShop.photos.groups["0"].items["0"].suffix;
-			console.log(coffeeShopPicture);
-			coffeeShops.push({
+
+			var coffeeShopObj = {
 				id : coffeeShop.id,
 				name : coffeeShop.name,
 				distance : coffeeShopDistance,
 				isOpen  : coffeeShopOpen(),
 				picture : coffeeShopPicture,
-				price : coffeeShopPrice()
-			});
-			console.log(coffeeShops);
+				price : coffeeShopPrice(),
+				lat : coffeeShop.location.lat,
+				lng : coffeeShop.location.lng
+			}
+			coffeeShops.push(coffeeShopObj);
+			sortByClosest();
+			createShopMarker(coffeeShopObj);
 		}
 	};
 	ajax.open("GET",  venueURL, true);
 	ajax.send();
 }
 
-setTimeout(function() {
-	sortByClosest();
-},3000);
-
 //// SORT FUNCTIONS
 function sortByClosest() {
 	coffeeShops.sort(function(a,b) {
 		return a.distance - b.distance;
 	});
-	console.log(coffeeShops);
 }
 
 function sortByCheapest() {
 	coffeeShops.sort(function(a,b) {
 		return a.price - b.price;
 	});
-	console.log(coffeeShops);
+}
+
+
+//GOOGLE MAPS API
+function initMap(userLatitude, userLongitude) {
+	map = new google.maps.Map(document.getElementById('google-map'), {
+		zoom:  15,
+		streetViewControl: false,
+		mapTypeControl: false,
+		center: {lat: userLatitude, lng: userLongitude}
+	});
+
+}
+function createShopMarker(coffeeShop) {
+	var marker = new google.maps.Marker({
+		position: {lat: coffeeShop.lat, lng: coffeeShop.lng},
+		map: map,
+		icon: "./assets/coffee-icon.png",
+		title: coffeeShop.name
+	});
+	var popupInfo = new google.maps.InfoWindow({
+		content: "<h2>" + coffeeShop.name + "("  + coffeeShop.distance + "m)"+ "</h2>"
+	});
+	marker.addListener("click", function() {
+		popupInfo.open(map, marker);
+	});
+	map.addListener("click", function() {
+		popupInfo.close();
+	});
+}
+function createUserMarker(userLat, userLng) {
+	var marker = new google.maps.Marker({
+		position: {lat: userLat, lng: userLng},
+		map: map,
+		icon: "./assets/user-icon.png",
+	});
 }
